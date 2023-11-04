35f6ab6e497162ce9c9a7b528a6c705b18e185ba
Pushkar Kumar Gautam <pushkarkumargautam@pop-os.localdomain>
2023-11-04 23:21:51 +0530

    Second commit, got everything setup,

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import {Button} from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <p className='text-red-500 font-bold'>Hello Notion Clone</p>
+    <div>
+      <Button variant="destructive">
+        Delete
+      </Button>
+    </div>
   )
 }
