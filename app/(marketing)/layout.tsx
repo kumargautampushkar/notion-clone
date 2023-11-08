39c030a9ee8f2b3ec017b575c230f0c6a84bbf5d
Pushkar Kumar Gautam <pushkarkumargautam@pop-os.localdomain>
2023-11-09 01:43:11 +0530

    Added padding after navbar

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,12 +3,12 @@ import { Navbar } from "./_components/navbar";
 const MarketingLayout=({
     children
 }:{
-    children: React.ReactNode
+    children: React.ReactNode;
 }) => {
     return(
         <div className="h-full">
             <Navbar></Navbar>
-            <main className="h-full">
+            <main className="h-full pt-40">
                 {children}
             </main>
 
