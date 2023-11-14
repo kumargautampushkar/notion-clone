6dbaaf4ef484e3968ad81084529851298c885c72
Pushkar Kumar Gautam <pushkarkumargautam@pop-os.localdomain>
2023-11-14 16:49:06 +0530

    Dynamic rendering in homepage wether authenticated or not

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,8 +2,15 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
+
 
 export const Heading = ()=>{
+    const {isAuthenticated, isLoading}= useConvexAuth(); 
+
     return (
         <div className="max-w-3xl space-y-4" >
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,9 +20,27 @@ export const Heading = ()=>{
             <h3 className="text-base sm-text-xl md:text-2xl font-medium ">
                 Jotion is a connected workspace where <br></br>better, faster work happens
             </h3>
-            <Button>
-                Enter Jotion <ArrowRight className="h-4 w-4 ml-2"></ArrowRight>
-            </Button>
+            {isLoading &&(
+                <div className="w-full flex items-center justify-center">
+                     <Spinner size="lg"></Spinner>
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href='/documents'>
+                        Enter Jotion <ArrowRight className="h-4 w-4 ml-2"></ArrowRight>
+                    </Link>
+                </Button>
+            )}
+
+            {!isAuthenticated && !isLoading &&(
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Jotion Free
+                        <ArrowRight></ArrowRight>
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     )
 }
