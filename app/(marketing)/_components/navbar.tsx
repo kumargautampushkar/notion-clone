e9300d9cad8fae46340fd03215dddb9282814571
Pushkar Kumar Gautam <pushkarkumargautam@pop-os.localdomain>
2023-11-14 16:22:00 +0530

    authorization setup succesfull and is working

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,15 @@ import { useScrollTop } from "@/hooks/use-scroll-top"
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton,UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { User } from "lucide-react";
 
 export const Navbar = ()=>{
+    const {isAuthenticated, isLoading} = useConvexAuth();
     const scrolled = useScrollTop();
     return (
         <div className={cn(
@@ -14,6 +21,33 @@ export const Navbar = ()=>{
         )}>
             <Logo></Logo>
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner></Spinner>
+                )}
+                {!isAuthenticated && !isLoading &&(
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size='sm'>
+                                Log In
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size='sm'>
+                                Get Jotion Free!
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Jotion
+                            </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/"></UserButton>
+                    </>
+                )}
                 <ModeToggle></ModeToggle>
             </div>
         </div>
